44f0afce28515a81ac1fd4499871b2292e27383e
CLaudia Luca <35894479+claudia-luca@users.noreply.github.com>
2018-03-02 14:53:28 +0100

    SQL Errors
    Learning SQL we come across many errors and we can learn a lot from this errors.

---
+++ SQL Errors.sql
@@ -0,0 +1,22 @@
+--Below are the errors which we come across very often.
+
+--ORA-00904: invalid identifier
+--ORA-00905: missing keyword
+--ORA-00917: missing comma
+--ORA-00918: column ambiguously defined
+--ORA-00923: FROM keyword not found where expected
+--ORA-00933: SQL command not properly ended
+--ORA-00934: group function is not allowed here
+--ORA-00936: missing expression
+--ORA-00942: table or view does not exist
+--ORA-00947: not enough values
+--ORA-00955: name is already used by an existing object
+--ORA-00979: not a GROUP BY expression
+--ORA-01441: cannot decrease column length because some value is too big
+--ORA-01722: invalid number
+--ORA-01789: query block has incorrect number of result columns
+--ORA-01790: expression must have same datatype as corresponding expression
+--ORA-01839: date not valid for month specified
+--ORA-01843: not a valid month
+--ORA-02260: table can have only one primary key
+--ORA-12899: value too large for column
